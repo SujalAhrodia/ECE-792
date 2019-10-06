623cc91e3a1478fe838f0b22de92b7ebb9dd90ee
Sujal Ahrodia <sujalahrodia@gmail.com>
2019-10-06 18:35:42 -0400

    mem+cpu sorted

--- HW2/usage.py
+++ HW2/usage.py
@@ -14,31 +14,50 @@ mems = {}
 j=0
 
 for i in vm:
-    stats = cl.lookupByID(i).getCPUStats(True)[0]
-    ctime = (float(stats['cpu_time'])/1000000000)
-    stime = (float(stats['system_time'])/1000000000)
-    utime = (float(stats['user_time'])/1000000000)
+    cstats = cl.lookupByID(i).getCPUStats(True)[0]
+    mstats = cl.lookupByID(i).memoryStats()
 
-    usage = float(100*(utime+stime)/ctime)
-    cpus[j] = cl.lookupByID(i).name(),usage
+    #CPU times
+    ctime = (float(cstats['cpu_time'])/1000000000)
+    stime = (float(cstats['system_time'])/1000000000)
+    utime = (float(cstats['user_time'])/1000000000)
+
+    #Memory Stats
+    cmem = float(mstats['rss'])
+    tmem = float(mstats['actual'])
+
+    #Memory Usage
+    musage = 100*float(cmem/tmem)
+    mems[j] = cl.lookupByID(i).name(),musage
+
+    #CPU usage
+    cusage = 100*float((utime+stime)/ctime)
+    cpus[j] = cl.lookupByID(i).name(),cusage
+
     j=j+1
 
 # list of sorted cpus
 sorted_cpus= []
 
+#list of sorted mems
+sorted_mems = []
+
 # sorting
 sorted_cpus = sorted(cpus.items(), key=lambda x: x[1][1])
+sorted_mems = sorted(mems.items(), key=lambda x: x[1][1])
 
-print ("------------------")
-print ("Sorted List of VMS:")
-print ("------------------")
-for i in sorted_cpus:
-    print ("VM Name: "+ str(i[1][0]))
-    print ("CPU Usage: "+ str(i[1][1])+ "%" )
+if sys.argv[1] == "CPU":
+    print ("------------------")
+    print ("Sorted List of VMS:")
+    print ("------------------")
+    for i in sorted_cpus:
+        print ("VM Name: "+ str(i[1][0]))
+        print ("CPU Usage: "+ str(i[1][1])+ "%" )
 
-
-
-# for i in range(0,len(vm)):
-#     gstate, gmmem, gmem, gcpu, gcput = cl.lookupByID(vm[i]).info()
-#     vms[i] = [vm[i],cl.lookupByID(vm[i]).name(), gcpu, gmem]
-
+if sys.argv[1] == "MEM":
+    print ("------------------")
+    print ("Sorted List of VMS:")
+    print ("------------------")
+    for i in sorted_mems:
+        print ("VM Name: "+ str(i[1][0]))
+        print ("MEM Usage: "+ str(i[1][1])+ "%" )
