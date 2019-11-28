d746e81e61a06a0162d67fd242fa9230962e504b
t11_vm4 <adharshsrivats@gmail.com>
2019-11-28 00:43:22 +0000

    L2 network

--- HW4/q2/main.py
+++ HW4/q2/main.py
@@ -1,17 +1,25 @@
+#LOGIC LAYER
 import os
 import sys
 import csv
 
 file_name = sys.argv[1]
 
-def deploy_cont(cont):
-    os.system("ansible-playbook create_container.yml --extra-vars cname="+cont)
-
-
 with open(file_name) as f:
     csv_reader = csv.reader(f, delimiter=',')
     line_count = 0
     for row in csv_reader:
         con1, con2, net = row[0], row[1], row[2]
-        deploy_cont(con1)
-        deploy_cont(con2)
+        os.system("ansible-playbook create_container.yml --extra-vars cname="+con1)
+        os.system("ansible-playbook create_container.yml --extra-vars cname="+con2)
+        if net.lower()=="bridge":
+            os.system("ansible-playbook create_L2.yml --extra-vars nname="+net+" c1name="+con1+" c2name="+con2)
+        elif net.lower()=="vxlan":
+             os.system("ansible-playbook create_VXLAN.yml --extra-vars nname="+net+" c1name="+con1+" c2name="+con2)
+        elif net.lower()=="l3":
+             os.system("ansible-playbook create_L3.yml --extra-vars nname="+net+" c1name="+con1+" c2name="+con2)
+        elif net.lower()=="gre":
+             os.system("ansible-playbook create_GRE.yml --extra-vars nname="+net+" c1name="+con1+" c2name="+con2)
+        else:
+            print("NOT A VALID NETWORK OPTION!")
+
